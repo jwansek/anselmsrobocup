aaf422b9c4db2a67974c4bdbd9431557dd1f7d4f
jwansek <eddie.atten.ea29@gmail.com>
2018-04-01 13:21:49 +0000

    added large_text.py file

---
+++ large_text.py
@@ -0,0 +1,27 @@
+#from http://www.network-science.de/ascii/
+#select font 'basic'
+
+class Text:
+    LINE_DETECTED = """
+db      d888888b d8b   db d88888b 
+88        `88'   888o  88 88'     
+88         88    88V8o 88 88ooooo 
+88         88    88 V8o88 88~~~~~ 
+88booo.   .88.   88  V888 88.     
+Y88888P Y888888P VP   V8P Y88888P 
+                                  
+                                  
+d8888b. d88888b d888888b d88888b  .o88b. d888888b d88888b d8888b. 
+88  `8D 88'     `~~88~~' 88'     d8P  Y8 `~~88~~' 88'     88  `8D 
+88   88 88ooooo    88    88ooooo 8P         88    88ooooo 88   88 
+88   88 88~~~~~    88    88~~~~~ 8b         88    88~~~~~ 88   88 
+88  .8D 88.        88    88.     Y8b  d8    88    88.     88  .8D 
+Y8888D' Y88888P    YP    Y88888P  `Y88P'    YP    Y88888P Y8888D'"""
+    
+    BALL_CLOSE = """
+d8888b.  .d8b.  db      db         .o88b. db       .d88b.  .d8888. d88888b 
+88  `8D d8' `8b 88      88        d8P  Y8 88      .8P  Y8. 88'  YP 88'     
+88oooY' 88ooo88 88      88        8P      88      88    88 `8bo.   88ooooo 
+88~~~b. 88~~~88 88      88        8b      88      88    88   `Y8b. 88~~~~~ 
+88   8D 88   88 88booo. 88booo.   Y8b  d8 88booo. `8b  d8' db   8D 88.     
+Y8888P' YP   YP Y88888P Y88888P    `Y88P' Y88888P  `Y88P'  `8888Y' Y88888P"""
