f2c91b5fe8b505ec878dceaeeb9adf9f51d4f3ad
jwansek <eddie.atten.ea29@gmail.com>
2018-07-19 12:18:06 +0100

    added compass implementaion

--- line_det/line_det.ino
+++ line_det/line_det.ino
@@ -1,3 +1,12 @@
+#include <Wire.h>
+//setup compass stuff
+int HMC6352Address = 0x42;
+// This is calculated in the setup() function
+int slaveAddress;
+byte headingData[2];
+int i, headingValue;
+int initcompass;
+
 // These constants won't change.  They're used to give names
 // to the pins used:
 //############  Pins #############
@@ -13,8 +22,15 @@ const int p_cap_echo = 53;
 
 void setup() 
 {
+  // Shift the device's documented slave address (0x42) 1 bit right
+  // This compensates for how the TWI library only wants the
+  // 7 most significant bits (with the high bit padded with 0)
+  slaveAddress = HMC6352Address >> 1;   // This results in 0x21 as the address to pass to TWI
+  
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
+  Wire.begin();
+  initcompass = get_compass_reading();  //get initial compass value to calculate correction heading
 
   pinMode(p_cap_trig, OUTPUT);
   pinMode(p_cap_echo, INPUT);
@@ -27,30 +43,33 @@ void loop()
   //Serial.print(get_pt_reading());
   Serial.print(0);
   Serial.print("\t");
+  
   //reading of the left ultrasonic sensor
   Serial.print(60);
   Serial.print("\t");
+  
   //reading of the right ultrasonic sensor
   Serial.print(60);
   Serial.print("\t");
+  
   //reading of the compass sensor
-  Serial.print(15);
+  int headingcorrection = initcompass - get_compass_reading();
+  Serial.print(headingcorrection);
   Serial.print("\t");
+  
   //if the switch is in the 'on' position
   Serial.print(0);
   Serial.print("\t");
+  
   //Reading of the capture detection
-  
-  Serial.println(get_US_reading(p_ping_cap, p_echo_cap));
   //Serial.println(0);
-  
   Serial.println(ball_in_capture());
   
     
   // wait 64 milliseconds before the next loop
   // for the analog-to-digital converter to settle
   // after the last reading:
-  delay(128);
+  delay(64);
 }
 
 int get_pt_reading()
@@ -88,4 +107,26 @@ int ball_in_capture()
     return 1;
   else
     return 0;
+}
+
+int get_compass_reading()
+{
+  // Send a "A" command to the HMC6352
+  // This requests the current heading data
+  Wire.beginTransmission(slaveAddress);
+  Wire.write("A");              // The "Get Data" command
+  Wire.endTransmission();
+  //delay(10);                   // The HMC6352 needs at least a 70us (microsecond) delay
+  // Read the 2 heading bytes, MSB first
+  // The resulting 16bit word is the compass heading in 10th's of a degree
+  // For example: a heading of 1345 would be 134.5 degrees
+  Wire.requestFrom(slaveAddress, 2);        // Request the 2 byte heading (MSB comes first)
+  i = 0;
+  while(Wire.available() && i < 2)
+  { 
+    headingData[i] = Wire.read();
+    i++;
+  }
+  headingValue = headingData[0]*256 + headingData[1];  // Put the MSB and LSB together
+  return int(headingValue / 10);
 }
